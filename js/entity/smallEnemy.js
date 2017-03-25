b72fe63f235a444d61d437fe6e512572fede8989
Tommy <tommynielsen67@gmail.com>
2017-03-25 21:46:10 +0100

    InitialCommit

---
+++ js/entity/smallEnemy.js
@@ -0,0 +1,188 @@
+class smallEnemy extends Phaser.Sprite {
+    constructor(game, posx, posy, key, type, properties, difficulty) {
+        super(game, posx, posy, 'Starships', 0, type, properties, difficulty);
+        game.add.existing(this);
+        game.physics.arcade.enable(this);
+        this.body.collideWorldBounds = true;
+        this.anchor.setTo(0.5, 0.5);
+        this.body.drag.set(0.5);
+        this.enemyShipProperties = properties;
+        if (type === undefined) {
+            type = Math.random() * (5 - 0) + 0;
+            type = Math.floor(type);
+        }
+        console.log('Enemy ship spawned! type is: ' + type + ' - Name: ' + this.enemyShipProperties[type][0] + ' - Speed is: ' + this.enemyShipProperties[type][2]);
+        this.animations.add('enemyAnim', [type]);
+        this.animations.play('enemyAnim');
+        this.difficulty = difficulty;
+        if (this.enemyShipProperties[type][5] === 1) {
+            this.turretEnabled = true;
+            this._addenemyGun();
+        } else {
+            this.turretEnabled = false;
+        }
+
+        this._addEmitter();
+        this.SPEED = this.enemyShipProperties[type][2] / this.difficulty; // missile speed pixels/second
+        this.TURN_RATE = this.enemyShipProperties[type][3] / this.difficulty; // turn rate in degrees/frame
+        this.playerX = 100;
+        this.playerY = 100;
+        this.body.bounce.set(0.4);
+        this.body.setSize(this.width - 8, this.width - 8, 8, 8);
+        this._addLaser();
+        this._initBullets();
+        this.fireRate = this.enemyShipProperties[type][6] * this.difficulty;
+        this._nextFire = 0;
+        this.alive = false;
+        this.game.time.events.add(Phaser.Timer.SECOND * 0.8, function () {
+            if(this.health > 0){
+            this.alive = true;
+            }
+        }, this);
+        this.health = this.enemyShipProperties[type][4] / this.difficulty;
+    }
+
+    _damageTaken(damage) {
+        this.health -= damage;
+        if (this.health < 0) {
+            this.alive = false;
+            this.emitter.on = false;
+            this._deathEmitter();
+            this.game.time.events.add(Phaser.Timer.SECOND * 4, function () {
+                this._targetReticule.destroy();
+                this.destroy();
+            }, this);
+        }
+    }
+
+    _addenemyGun() {
+        this.enemyGun = this.game.add.image(0, 0, 'gun');
+        this.enemyGun.anchor.setTo(0.5);
+    }
+
+    _fireWeapon() {
+        this.bullet;
+        this._nextFire = this.game.time.now + this.fireRate;
+        this.bullet = this.bullets.getFirstDead();
+        this.bullet.reset(this.x, this.y);
+        if (this.turretEnabled) {
+            this.game.physics.arcade.velocityFromAngle(this._targetReticule.angle, 900, this.bullet.body.velocity);
+            this.bullet.rotation = this._targetReticule.rotation;
+        } else {
+            this.game.physics.arcade.velocityFromAngle(this.angle, 900, this.bullet.body.velocity);
+            this.bullet.angle = this.angle;
+        }
+        this.bullet.bringToTop();
+        this.bullets.add(this.bullet);
+
+    }
+
+    _initBullets() {
+        this.bullets = this.game.add.group();
+        this.bullets.enableBody = true;
+        this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+        this.bullets.createMultiple(500, 'bullet');
+        this.bullets.setAll('checkWorldBounds', true);
+        this.bullets.setAll('outOfBoundsKill', true);
+        this.bullets.setAll('anchor.x', 0.5);
+        this.bullets.setAll('anchor.y', 0.5);
+        //  --- Disable Gravity for Each Bullet
+        this.bullets.forEach(function (L) {
+            L.body.allowGravity = false;
+        })
+    }
+
+    _addLaser() {
+        this._targetReticule = this.game.add.tileSprite(0, 0, 800, 0.5, 'redpointer');
+        this._targetReticule.anchor.setTo(0.0, 0.5);
+        this._targetReticule.visible = false;
+    }
+    _addEmitter() {
+        this.emitter = this.game.add.emitter(0, 0, 200);
+        this.emitter.width = 0;
+        this.emitter.makeParticles('flame');
+        this.emitter.maxParticleSpeed = new Phaser.Point(-100, 50);
+        this.emitter.minParticleSpeed = new Phaser.Point(-200, -50);
+        this.emitter.minParticleScale = 0.5;
+        this.emitter.maxParticleScale = 8.9;
+        this.emitter.setRotation(0, 190);
+        this.emitter.setAlpha(0.1, 0.6);
+        this.emitter.forEach(function (particle) {
+            particle.body.allowGravity = false;
+
+        }, this);
+        this.emitter.setScale(0.3, 2, 0.3, 2, 200);
+        this.emitter.start(false, 200, 1);
+        this.addChild(this.emitter);
+        this.emitter.on = false;
+        this.emitter.y = 0;
+        this.emitter.x = -6;
+    }
+
+    _deathEmitter() {
+        this.deathEmitter = this.game.add.emitter(0, 0, 0);
+        this.addChild(this.deathEmitter);
+        this.deathEmitter.width = 0;
+        this.deathEmitter.makeParticles('flame');
+        this.deathEmitter.setRotation(0, 190);
+        this.deathEmitter.setAlpha(0.1, 1);
+        this.deathEmitter.forEach(function (particle) {
+            particle.body.allowGravity = false;
+        }, this);
+        this.deathEmitter.setScale(0.3, 2.5, 0.3, 2.5, 400);
+        //this.deathEmitter.start(false, 800, 100);
+        this.deathEmitter.start(false, 400, 200);
+        this.deathEmitter.on = true;
+    }
+
+    update() {
+        if (this.alive) {
+            this._targetReticule.x = this.x;
+            this._targetReticule.y = this.y;
+            var storedAngle = this.game.physics.arcade.angleToXY(this._targetReticule, this.playerX, this.playerY);
+            this._targetReticule.rotation = storedAngle;
+            var storedShipAngle = Math.abs(this.rotation);
+            var storedPointerAngle = Math.abs(this._targetReticule.rotation);
+            if (storedPointerAngle < storedShipAngle + 0.2 && storedPointerAngle > storedShipAngle - 0.2 && this.game.time.now > this._nextFire && this.turretEnabled === false) {
+                this._fireWeapon();
+            } else if (this.game.time.now > this._nextFire && this.turretEnabled) {
+                this._fireWeapon();
+            }
+
+            //
+            //                        if (storedPointerAngle < storedShipAngle + 0.2 && storedPointerAngle > storedShipAngle - 0.2 && this.game.time.now > this._nextFire) {
+            //                            this._fireWeapon();
+            //                        }
+
+            this.targetDistance = this.game.math.distance(this.x, this.y, this.playerX, this.playerY);
+            var targetAngle = this.game.math.angleBetween(
+                this.x, this.y,
+                this.playerX, this.playerY
+            );
+
+            var delta = targetAngle - this.rotation;
+            if (delta > Math.PI) delta -= Math.PI * 2;
+            if (delta < -Math.PI) delta += Math.PI * 2;
+
+            if (this.targetDistance > 120) {
+                this.game.physics.arcade.accelerationFromRotation(this.rotation, this.SPEED, this.body.acceleration);
+                this.emitter.on = true;
+            } else {
+                this.emitter.on = false;
+            }
+
+            if (delta > 0) {
+                this.angle += this.TURN_RATE;
+            } else {
+                this.angle -= this.TURN_RATE;
+            }
+
+        } else {
+            this.angle += 2;
+
+        }
+
+    }
+
+
+}
---
+++ js/map/mainmenu.js
@@ -0,0 +1,177 @@
+class MainMenu extends Phaser.State {
+    constructor() {
+        super();
+        this.textTimer = 0;
+        this.startTextAlive = false;
+        this.selectedShip = 1;
+        this.nameArray = ['Badger', 'Orsus', 'Raven', 'SwiftWind', 'Brick'];
+        this.descriptionArray = [
+            'Heavy Armor - Slow Movement',
+            'Best rate of fire.',
+            'Best Speed and maneuverability',
+            'Speedy and equipped with a turret',
+            'Slow with a powerful turret.'
+        ];
+        if(this._difficulty === undefined){
+        this._difficulty = 2;}
+    }
+
+
+    _loadMenuItems() {
+        this.titleBackground = this.game.add.image(0, 0, 'titleBackground');
+        this.titleBackground.alpha = 0.0;
+        this.tween = game.add.tween(this.titleBackground);
+        this.tween.to({
+            y: -90,
+            alpha: 1.0
+        }, 5000, 'Linear', true, 0);
+        this.game.time.events.add(Phaser.Timer.SECOND * 1, function () {
+            this._addTitleText();
+        }, this);
+    }
+
+
+    _loadGameOptions() {
+        this.settingsMenu = this.game.add.image(460, 300, 'gameSettings');
+        this.settingsMenu.anchor.setTo(0.5);
+        this.instructions = this.game.add.button(280, 172, 'easyDifficulty', function () {
+            this._difficulty = 2;
+        }, this, 1, 2, 0);
+        this.instructions = this.game.add.button(410, 172, 'normalDifficulty', function () {
+            this._difficulty = 1.5;
+        }, this, 1, 2, 0);
+        this.instructions = this.game.add.button(560, 172, 'hardDifficulty', function () {
+            this._difficulty = 1;
+        }, this, 1, 2, 0);
+        this.shipPlacementX = 222;
+        this.placementX = 210;
+        for (var i = 0; i <= 4; i++) {
+            this.placementX += 74;
+            this.shipPlacementX += 74;
+            let buttonNumber = i;
+            this.ship = this.game.add.button(this.placementX, 272, 'checkButton', function () {
+                this._shipHandler(buttonNumber);
+            }, this, 1, 2, 2, 2);
+
+            this.shipPreview = this.game.add.sprite(this.shipPlacementX, 284, 'UiShips');
+            this.shipPreview.frame = i;
+        }
+
+
+
+        this.shipName = this.game.add.text(460, 360, this.nameArray[this.selectedShip], {
+            font: "16px Press Start 2P",
+            fill: '#ffffff',
+            boundsAlignH: "center"
+        });
+        this.shipName.anchor.setTo(0.5);
+
+
+        this.shipDescription = this.game.add.text(460, 420, this.descriptionArray[this.selectedShip], {
+            font: "12px Press Start 2P",
+            fill: '#ffffff',
+            boundsAlignH: "center"
+        });
+        this.shipDescription.visible = false;
+        this.shipDescription.anchor.setTo(0.5);
+
+        this.beginButton = this.game.add.button(340, 540, 'begin', function () {
+            //this.game.state.states['SimpleLevel'].finalShipSelection = 2;
+            this.game.state.start('SimpleLevel', true, false, this.selectedShip, this._difficulty);
+        }, this, 1, 2, 0);
+
+
+
+    }
+
+    _shipHandler(input) {
+        this.shipDescription.visible = true;
+        this.selectedShip = input;
+        this.shipName.setText(this.nameArray[input]);
+        this.shipDescription.setText(this.descriptionArray[input]);
+
+    }
+
+    _loadMainMenu() {
+        this.startTextAlive = false;
+        this.startText.destroy();
+        this.startMenuImage = this.game.add.image(460, 360, 'titleMenuImage');
+        this.startMenuImage.anchor.setTo(0.5);
+        this.startNewGame = this.game.add.button(460, 346, 'startNewGame', function () {
+            //this.game.state.start('SimpleLevel');
+            this._loadGameOptions();
+        }, this, 1, 2, 0);
+        this.startNewGame.anchor.setTo(0.5);
+        this.copyright = this.game.add.text(830, 620, '2017 Tommy Nielsen', {
+            font: "8px Press Start 2P",
+            fill: '#ffffff',
+            boundsAlignW: "right"
+        });
+        this.copyright.anchor.setTo(0.5, 0.0);
+        this.versionText = this.game.add.text(460, 490, '(Mouse strongly recommended)', {
+            font: "8px Press Start 2P",
+            fill: '#ffffff',
+            boundsAlignH: "center"
+        });
+        this.versionText.anchor.setTo(0.5);
+        this.instructions = this.game.add.button(460, 374, 'instructions', function () {
+            this._loadInstructions();
+        }, this, 1, 2, 0);
+        this.instructions.anchor.setTo(0.5);
+    }
+
+    _loadInstructions() {
+        this.instructions = this.game.add.image(460, 330, 'gameInstructions');
+        this.instructions.anchor.setTo(0.5);
+        this.instructions.inputEnabled = true;
+        this.instructions.events.onInputDown.add(function () {
+            this.instructions.destroy();
+
+
+        }, this);
+    }
+
+    _addTitleText() {
+        this.startTextAlive = true;
+        this.starBurstText = this.game.add.text(460, 240, 'STARBURST', {
+            font: "36px Press Start 2P",
+            fill: '#ffffff',
+            boundsAlignH: "center"
+        });
+
+        this.startText = this.game.add.text(460, 340, '... Press To Begin ...', {
+            font: "16px Press Start 2P",
+            fill: '#ffffff',
+            boundsAlignH: "center"
+        });
+        this.startText.visible = false;
+
+        this.starBurstText.anchor.setTo(0.5);
+        this.startText.anchor.setTo(0.5);
+    }
+
+    preload() {}
+
+    create() {
+        this._loadMenuItems();
+
+    }
+
+    update() {
+        this.textTimer += this.game.time.elapsed;
+        if (this.textTimer >= 1000 && this.startTextAlive) {
+
+            this.textTimer -= 1000;
+            this.startText.visible = !this.startText.visible;
+        }
+
+        if (this.game.input.activePointer.leftButton.isDown && this.startTextAlive) {
+
+            this._loadMainMenu();
+        }
+
+
+
+    }
+
+}
