6778f0d8f32d09005e1323b90c6d7909f911b821
Tommy <tommynielsen67@gmail.com>
2017-03-26 19:11:18 +0200

    Modifying direction marker

--- js/map/simplelevel.js
+++ js/map/simplelevel.js
@@ -1,7 +1,7 @@
 class SimpleLevel extends Phaser.State {
     constructor(selectedShip) {
         super(selectedShip);
-     
+
     }
 
     _loadLevel() {
@@ -10,178 +10,37 @@ class SimpleLevel extends Phaser.State {
             e.preventDefault();
         }
         this.game.world.setBounds(0, 0, 920, 640);
-        this.background = game.add.tileSprite(0, 0, 920, 640, 'background');
-        this.overlay = game.add.tileSprite(-300, -300, 1020, 740, 'Overlay');
-        this.overlay2 = game.add.tileSprite(-300, -300, 1020, 740, 'Overlay2');
-    
+        console.log('Load Level fired!');
+        //        this.background = game.add.tileSprite(0, 0, 920, 640, 'background');
+        //        this.overlay = game.add.tileSprite(-300, -300, 1020, 740, 'Overlay');
+        //        this.overlay2 = game.add.tileSprite(-300, -300, 1020, 740, 'Overlay2');
+
     }
 
     _addPlayer(x, y) {
-        //this.player = new Player(this.game, x, y, 'starships');
-        this.player = new Player(this.game, this.game.width / 2, this.game.height / 2, 'starships', this.shipType, this.shipProperties);
-        this.player.shipProperties = this.shipProperties;
+        this.player = new Player(this.game, this.game.width / 2, this.game.height / 2, 'bulwark');
         this.game.camera.follow(this.player);
     }
 
     _loadUi() {
-        this.userInterface = new UserInterface(this.game, this._difficulty, this.shipType /* HERE !!!! */);
-
+        //        this.userInterface = new UserInterface(this.game, this._difficulty, this.shipType /* HERE !!!! */);
     }
 
-    _enemy_hit(bullet, enemy) {
-
-        bullet.kill();
-        enemy._damageTaken(16);
-        enemy.body.velocity.x = bullet.body.velocity.x / 8;
-        enemy.body.velocity.y = bullet.body.velocity.y / 8;
-        this.explosion.x = enemy.x;
-        this.explosion.y = enemy.y;
-        this.explosion.on = true;
-        this.game.time.events.add(Phaser.Timer.SECOND * 0.3, this._endExplosion, this);
-        if (enemy.health < 15 && enemy.alive) {
-            this.userInterface._updateScore(20);
-        }
-    }
-
-    _player_hit(player, bullet) {
-        bullet.kill();
-        player.body.velocity.x = bullet.body.velocity.x / 8;
-        player.body.velocity.y = bullet.body.velocity.y / 8;
-        this.explosion.x = player.x;
-        this.explosion.y = player.y;
-        this.explosion.on = true;
-        this.game.camera.shake(0.06, 20);
-        this.userInterface._updateDamage(10);
-        this.game.time.events.add(Phaser.Timer.SECOND * 0.3, this._endExplosion, this);
-
-        if (this.userInterface.health < 0 && this.player.alive) {
-            this.player.alive = false;
-            var deathTimer = Math.random() * (9 - 5) + 5;
-            this.player._playerDeath(deathTimer);
-            this.game.time.events.add(Phaser.Timer.SECOND * deathTimer, this._gameOver, this);
-        }
-    }
-
-    _gameOver() {
-        this.userInterface._gameOverMenu();
-    }
-
-    _endExplosion() {
-        this.explosion.on = false;
-    }
-
-    _addExplosion() {
-        this.explosion = this.game.add.emitter(0, 0, 200);
-        this.explosion.width = 0;
-        this.explosion.makeParticles('blueFlame');
-        this.explosion.minParticleSpeed.setTo(-200, -200);
-        this.explosion.maxParticleSpeed.setTo(200, 200);
-        this.explosion.setRotation(0, 190);
-        this.explosion.setAlpha(0.1, 1);
-        this.explosion.forEach(function (particle) {
-            particle.body.allowGravity = false;
-            particle.animations.add('emit1', [0]);
-            particle.animations.add('emit2', [1]);
-            particle.animations.add('emit3', [2]);
-            var randSpeed = Math.random() * (4 - 0) + 0;
-            var randSpeed = Math.floor(randSpeed);
-            if (randSpeed === 1) {
-                particle.animations.play('emit1', 30, true);
-            } else if (randSpeed === 2) {
-                particle.animations.play('emit2', 30, true);
-            } else {
-                particle.animations.play('emit3', 30, true);
-            }
-        }, this);
-        this.explosion.setScale(0.3, 1, 0.3, 1, 160);
-        this.explosion.start(false, 160, 1);
-        this.explosion.on = false;
-    }
+    _checkCollision() {}
 
 
-    _checkCollision() {
-        this.game.physics.arcade.collide(this.player, this.enemies);
-        this.game.physics.arcade.collide(this.player.bullets, this.enemies, this._enemy_hit, null, this);
-        if (this.enemies.length > 0) {
-            this.game.physics.arcade.collide(this.enemy.bullets, this.player, this._player_hit, null, this);
-        }
-    }
 
-    _aiUpdater() {
-        var storedX = this.player.x;
-        var storedY = this.player.y;
-        this.enemies.forEach(function (enemy) {
-            enemy.playerX = storedX;
-            enemy.playerY = storedY;
-        }, this)
-    }
-
-    _nextWave() {
-        this.enemies = this.game.add.group();
-        this.roundTimerRunning = true;
-        this.currentWave++;
-        if (this.currentWave % 3 === 0) {
-            this.enemyWaveMultiplier++;
-        }
-        this.userInterface._waveComplete();
-        this.game.time.events.add(Phaser.Timer.SECOND * 10, function () {
-            for (this.i = 0; this.i < this.enemyWaveMultiplier; this.i++) {
-                var randomX = Math.random() * (900 - 20) + 20;
-                var randomY = Math.random() * (620 - 20) + 20;
-                this.enemy = new smallEnemy(this.game, randomX, randomY, 'player', /*undefined*/ undefined, this.shipProperties, this._difficulty);
-
-                this.enemies.add(this.enemy);
-            }
-            this.roundTimerRunning = false;
-            this.userInterface._waveComplete();
-            this.userInterface._updateWave();
-        }, this);
-    }
-  
-    init(selectedShip, difficulty) {
-    this.shipType = selectedShip;
-        if(this.shipType === undefined){
-            this.shipType = 1;
-        }
-    this._difficulty = difficulty;
-    }
+    init() {}
     preload() {}
 
     create() {
-
-        this.enemyWaveMultiplier = 1;
-        this.shipProperties = [
-      ['Badger', 0, 200, 2.5, 160, 0, 160],
-      ['Orsus', 1, 180, 2.5, 100, 0, 120],
-      ['Raven', 2, 280, 4, 140, 0, 180],
-  ['ShiftWind', 3, 220, 2.2, 120, 1, 400],
-      ['Brick', 4, 160, 2, 180, 1, 250]
-];
-        this.game.stage.smoothed = false;
-        this.enemies = this.game.add.group();
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        this.currentWave = 0;
-        this.roundTimerRunning = false;
         this._loadLevel();
         this._addPlayer(100, 100);
-        this._addExplosion();
-        this._loadUi();
+
     }
 
     update() {
-        this._aiUpdater();
-        if (this.player.alive) {
-            this.overlay.x = this.player.x * 0.16 - 100;
-            this.overlay.y = this.player.y * 0.16 - 100;
-            this.overlay2.x = this.player.x * 0.22 - 100;
-            this.overlay2.y = this.player.y * 0.22 - 100;
-            this._checkCollision();
 
-            if (this.enemies.length <= 0 && this.roundTimerRunning === false) {
-
-                this._nextWave();
-            }
-        }
     }
 
 }
--- js/preload.js
+++ js/preload.js
@@ -2,66 +2,27 @@ class Preload extends Phaser.State {
     preload() {
         //Load Webfont
         this.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
-        
-        
-        //TitleMenu items
-        this.load.image('gameSettings', 'img/titleMenu/gameSettings.png');
-        this.load.spritesheet('UiShips', 'img/titleMenu/menuTileSheet.png', 36, 28, 5);
-        this.load.image('titleBackground', 'img/titleMenu/titleBackground.png');
-        this.load.image('titleMenuImage', 'img/titleMenu/titleMenuImage.png');
-        this.load.spritesheet('startNewGame', 'img/titleMenu/newGameImage.png', 130, 18, 2);
-        this.load.spritesheet('instructions', 'img/titleMenu/instructionsImage.png', 194, 18, 2);
-        this.load.spritesheet('begin', 'img/titleMenu/fight.png', 238, 40, 2);
-        this.load.image('gameInstructions', 'img/titleMenu/gameInstructions.png');
-        
-        this.load.spritesheet('easyDifficulty', 'img/titleMenu/easyDifficulty.png', 66, 18, 2);
-        this.load.spritesheet('normalDifficulty', 'img/titleMenu/normalDifficulty.png', 98, 18, 2);
-        this.load.spritesheet('hardDifficulty', 'img/titleMenu/hardDifficulty.png', 66, 18, 2);
-        this.load.spritesheet('checkButton', 'img/titleMenu/checkButton.png', 56, 56, 2);
-        
-        // Images :
-        //this.load.image('player', 'img/Badger.png');
-        this.load.spritesheet('Starships', 'img/starshipTileSheet.png', 36, 28, 5);
-        this.load.image('bullet', 'img/bullet2.png');
-        this.load.image('gun', 'img/gun.png');
-        
-        
-        this.load.image('flame', 'img/flame.png');
-        this.load.image('background', 'img/testBackground.png');
-        this.load.image('Overlay', 'img/testOverlay.png');
-        this.load.image('Overlay2', 'img/testOverlay2.png');
-        this.load.spritesheet('blueFlame', 'img/blueFlameSpritesheet.png', 10, 10, 3);
-        this.load.spritesheet('redFlame', 'img/blueFlameSpritesheet.png', 10, 10, 3);
-
-
         // js scripts :
         this.load.script('player', 'js/entity/player.js');
         this.load.script('simpleLevel', 'js/map/simplelevel.js');
         this.load.script('MainMenu', 'js/map/mainmenu.js');
         this.load.script('smallEnemy', 'js/entity/smallEnemy.js');
         this.load.script('UserInterface', 'js/entity/userInterface.js');
+        
+        this.load.image('bulwark', 'img/bulwark.png');
+        this.load.image('bigArrow', 'img/bigArrow.png');
 
         // Pointers :
-         this.load.image('pointer', 'img/laserPointer.png');
-         this.load.image('redpointer', 'img/redlaserPointer.png');
-        
-        //Ui stuff
-        
-        this.load.image('statusUi', 'img/Ui/statusUi.png');
-        this.load.image('redPixel', 'img/Ui/redPixel.png');
-        this.load.image('bluePixel', 'img/Ui/bluePixel.png');
-        this.load.image('orangePixel', 'img/Ui/orangePixel.png');
-        this.load.image('gameOverMenu', 'img/Ui/gameOverMenu.png');
-        this.load.image('WaveComplete', 'img/Ui/waveComplete.png');
-        this.load.spritesheet('restartButton', 'img/Ui/GOrestart.png', 314, 46, 2);
-        this.load.spritesheet('menuButton', 'img/Ui/GOmenu.png', 282, 46, 2);
+//         this.load.image('pointer', 'img/laserPointer.png');
+//         this.load.image('redpointer', 'img/redlaserPointer.png');
+
     }
     create() {
         console.log("Preload.js:  Preload.create-> load_Level");
         this.game.state.add('SimpleLevel', SimpleLevel);
         this.game.state.add('MainMenu', MainMenu);
-        //this.game.state.start('SimpleLevel');
-        this.game.state.start('MainMenu');
+        this.game.state.start('SimpleLevel');
+        //this.game.state.start('MainMenu');
     }
 
 }
